4cc80347f020aec00ee8e8d0eb666918452fa72f
Harsh Kansara <harshkansara98@gmail.com>
2021-04-27 18:05:23 +1200

    created user and cart sagas for sign-in, sign-up and sign-in with google

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -30,6 +30,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                     cardItem => cardItem.id !== action.payload.id
                 )
             };
+        case CartActionTypes.CLEAR_CART:
+            return {
+                ...state,
+                cartItems: []
+            }
         default:
             return state;
     }
